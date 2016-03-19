45e8f095890a7576f318c3bdc3e6892fe5b12407
James Loewen <james.loewen@uwaterloo.ca>
2016-03-19 12:59:51 -0700

    Move callback to background script

---
+++ background.js
@@ -0,0 +1,32 @@
+window.setInterval(function () {
+    var date = new Date();
+    var year = String(date.getFullYear());
+    var month = String(date.getMonth() + 1);
+    if (date.getMonth() < 9) {
+        month = '0' + month;
+    }
+    var day = String(date.getDate());
+    if (date.getDate() < 10) {
+        day = '0' + day;
+    }
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+          //  console.log(JSON.parse(xhttp.responseText));
+        }
+    };
+    xhttp.open("GET", "http://mlb.mlb.com/gdcross/components/game/mlb/year_" + year + "/month_" + month + "/day_" + day + "/master_scoreboard.json", true);
+    xhttp.send();
+
+    var queryInfo = {
+        active: true,
+    }
+
+    chrome.tabs.query(queryInfo, function (tabs) {
+	var tab = tabs[0];
+        chrome.tabs.executeScript( tab.id, {
+            code: 'console.log("hello")'
+        });
+    });
+
+}, 1000);
--- content.js
+++ content.js
@@ -2,19 +2,3 @@
 var firstHref = $("a[href^='http']").eq(0).attr("href");
 
 console.log(firstHref);
-
-window.setInterval(function () {
-    var date = new Date();
-    var year = String(date.getFullYear());
-    var month = String(date.getMonth() + 1);
-    if (date.getMonth() < 9) {
-        month = '0' + month;
-    }
-    var day = String(date.getDate());
-    if (date.getDate() < 10) {
-        day = '0' + day;
-    }
-    $.get("http://mlb.mlb.com/gdcross/components/game/mlb/year_" + year + "/month_" + month + "/day_" + day + "/master_scoreboard.json", function (response) {
-        console.log(response);
-    });
-}, 1000);
