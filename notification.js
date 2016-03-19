835bf14311ab4dc95964524d6a315cc07db4ce90
Eli Smukler <eli@berecruited.com>
2016-03-19 15:32:59 -0700

    Remove notification after 2 seconds

--- notification.js
+++ notification.js
@@ -1,6 +1,10 @@
 var postNotification = function(text) {
     // TODO use text
   var body = $('body');
-  var notificationDiv = '<div id="created_div" style="position: fixed; top: 0; right: 0; padding: 10px; min-width: 200px; text-align: center; background: #ffffcc;"><p>SFG vs. OAK</p><p>' + text + '</p></div>'
+  var notificationDiv = '<div id="notifictation" style="position: fixed; top: 0; right: 0; padding: 10px; min-width: 200px; text-align: center; background: #ffffcc;"><p>SFG vs. OAK</p><p>' + text + '</p></div>'
   body.append(notificationDiv);
+
+  setTimeout(function() {
+      $('#notifictation').remove();
+  }, 2000);
 };
