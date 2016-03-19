3f9f3562c337a6bcb8b3693b3a2cfcd0915a8ac2
Eli Smukler <eli@berecruited.com>
2016-03-19 16:16:35 -0700

    Add url to notification

--- notification.js
+++ notification.js
@@ -1,10 +1,10 @@
-var postNotification = function(text) {
+var postNotification = function(url, text) {
     // TODO use text
   var body = $('body');
-  var notificationDiv = '<div id="notifictation" style="position: fixed; top: 0; right: 0; padding: 10px; min-width: 200px; text-align: center; background: #ffffcc;"><p>SFG vs. OAK</p><p>' + text + '</p></div>'
+  var notificationDiv = '<div id="mlbbaseball-notification" style="position: fixed; top: 0; right: 0; padding: 10px; min-width: 200px; text-align: center; margin: 8px; border-radius: 4px; font-size: 16px; background: #ddd; z-index: 1000000;"><p style="font-weight: 600; margin: 0;"><a href="' + url + '" target="_blank">SFG vs. OAK</a></p><p style="margin: 0;">' + text + '</p></div>'
   body.append(notificationDiv);
 
   setTimeout(function() {
-      $('#notifictation').remove();
-  }, 2000);
+      $('#mlbbaseball-notification').remove();
+  }, 5000);
 };
