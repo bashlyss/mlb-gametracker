448217f56be655bbbc7f73c75ca5186cc5054f4a
StoicLoofah <kkleung89@gmail.com>
2016-03-19 13:04:15 -0700

    adding local storage

--- popup.js
+++ popup.js
@@ -2,8 +2,17 @@ function submitForm(e) {
     // Cancel the form submit
     e.preventDefault();
     var form = $(e.target);
-    var data = form.serialize();
+    var data = form.serializeArray();
     console.log(data);
+
+    data.forEach(function(elem){
+        if(elem.name == 'event') {
+            var key = elem.value;
+            chrome.storage.local.set({key: true});
+            console.log(key);
+        }
+    });
+
 };
 
 // When the popup HTML has loaded
@@ -13,3 +22,5 @@ window.addEventListener('load', function(e) {
         $('form').on('submit', submitForm);
     }
 });
+
+
