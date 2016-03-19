049ab2812a363e5ac53e6c0a2e41767886fd8e22
Eli Smukler <eli@berecruited.com>
2016-03-19 15:04:11 -0700

    Add notification html

--- notification.js
+++ notification.js
@@ -1,5 +1,5 @@
 var postNotification = function() {
   var body = $('body');
-  body.append('<div id="created_div">hello this is a notification</div>');
+  var notificationDiv = '<div id="created_div" style="position: fixed; top: 0; right: 0; padding: 10px; min-width: 200px; text-align: center; background: #ffffcc;"><p>SFG vs. OAK</p><p>Here is the status!</p></div>'
+  body.append(notificationDiv);
 };
-
