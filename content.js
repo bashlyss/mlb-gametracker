a135acfb4aa2149e77c54238fc813303b2ebcf2f
StoicLoofah <kkleung89@gmail.com>
2016-03-19 10:44:00 -0700

    adding runners on base

--- content.js
+++ content.js
@@ -2,3 +2,26 @@
 var firstHref = $("a[href^='http']").eq(0).attr("href");
 
 console.log(firstHref);
+
+
+
+
+
+
+function getRunners() {
+    var bases = [];
+    if ($('.bases_container .base.first.active').length) {
+        bases.push(1);
+    }
+    if ($('.bases_container .base.second.active').length) {
+        bases.push(2);
+    }
+    if ($('.bases_container .base.third.active').length) {
+        bases.push(3);
+    }
+    return bases
+}
+
+window.setInterval(function(){
+    console.log('runners on base: ' + getRunners())
+}, 1000);
