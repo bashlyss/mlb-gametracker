aa6799c44e254cae8c05cbf04d2e5b4482f280ce
James Loewen <james.loewen@uwaterloo.ca>
2016-03-19 11:57:47 -0700

    Fix data retrieval

--- content.js
+++ content.js
@@ -6,15 +6,15 @@ console.log(firstHref);
 window.setInterval(function () {
     var date = new Date();
     var year = String(date.getFullYear());
-    var month = String(date.getMonth());
-    if (date.getMonth() < 10) {
+    var month = String(date.getMonth() + 1);
+    if (date.getMonth() < 9) {
         month = '0' + month;
     }
-    var day = String(date.getDay());
-    if (date.getDay() < 10) {
+    var day = String(date.getDate());
+    if (date.getDate() < 10) {
         day = '0' + day;
     }
-    $.get("http://mlb.mlb.com/gdcross/components/game/mlb/year_" + year + "/month_" + month + "/day_" + day + "/master_scoreboard,json", function (response) {
+    $.get("http://mlb.mlb.com/gdcross/components/game/mlb/year_" + year + "/month_" + month + "/day_" + day + "/master_scoreboard.json", function (response) {
         console.log(response);
     });
-});
+}, 1000);
