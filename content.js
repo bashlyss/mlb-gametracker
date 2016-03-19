c34379f902a3f01cc815b3672752edfeb88aafd7
James Loewen <james.loewen@uwaterloo.ca>
2016-03-19 14:48:09 -0400

    Retrieve game data from master_scoreboard

--- content.js
+++ content.js
@@ -3,3 +3,18 @@ var firstHref = $("a[href^='http']").eq(0).attr("href");
 
 console.log(firstHref);
 
+window.setInterval(function () {
+    var date = new Date();
+    var year = String(date.getFullYear());
+    var month = String(date.getMonth());
+    if (date.getMonth() < 10) {
+        month = '0' + month;
+    }
+    var day = String(date.getDay());
+    if (date.getDay() < 10) {
+        day = '0' + day;
+    }
+    $.get("http://mlb.mlb.com/gdcross/components/game/mlb/year_" + year + "/month_" + month + "/day_" + day + "/master_scoreboard,json", function (response) {
+        console.log(response);
+    });
+});
