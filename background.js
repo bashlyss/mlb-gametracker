6546809a115e6df0e06ca86a76474bfe5db5d652
James Loewen <james.loewen@uwaterloo.ca>
2016-03-19 13:51:23 -0700

    Provide game state from gameday data

--- background.js
+++ background.js
@@ -1,3 +1,65 @@
+function getGameState(game) {
+    return {
+        teams: {
+                   away: game.away_name_abbrev,
+                   home: game.home_name_abbrev,
+               },
+        runners: {
+                     first: game.runners_on_base.hasOwnProperty('runner_on_1b'),
+                     second: game.runners_on_base.hasOwnProperty('runner_on_2b'),
+                     third: game.runners_on_base.hasOwnProperty('runner_on_3b')
+         
+                 },
+        balls: parseInt(game.status.b),
+        strikes: parseInt(game.status.s),
+        outs: parseInt(game.status.o),
+    }
+}
+
+function getGameStateDiff(a, b) {
+    return _.reduce(a, function(result, value, key) {
+        return _.isEqual(value, b[key]) ?
+        result : result.concat(key);
+    }, []);
+}
+
+var gameState = null;
+var gameStateListeners = [];
+
+function checkGameState(game) {
+    var newGameState = getGameState(game);
+    var diff = getGameStateDiff(newGameState, gameState);
+    if(diff.length) {
+        if(diff.indexOf('strikes') != -1) {  // not sure why other lodash functions aren't working
+            _.filter(gameStateListeners, {'event': 'strike'}).forEach(function(listener) {
+                if(!listener.condition || listener.condition(newGameState.strikes, gameState.strike)) {
+                    listener.callback();
+                }
+            });
+        }
+        console.log(diff);
+    }
+    gameState = newGameState;
+}
+
+function bindGameListener(eventName, call, condition) {
+    gameStateListeners.push({'event': eventName, callback: call, condition: condition});
+}
+
+function triggerGameEvent(eventName, newGameEvent) {
+    _.filter(gameStateListeners, {'event': eventName}).forEach(function(listener) {
+        if(!listener.condition || listener.condition(newGameEvent.description)) {
+            listener.callback();
+        }
+    });
+}
+
+bindGameListener('strike', function(){console.log('STRIKKKEEE!');});
+bindGameListener('single', function(){console.log('SINGLE!SINGLE!');});
+bindGameListener('play', function(){console.log('There was a play!');});
+
+
+
 window.setInterval(function () {
     var date = new Date();
     var year = String(date.getFullYear());
@@ -12,21 +74,26 @@ window.setInterval(function () {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
-          //  console.log(JSON.parse(xhttp.responseText));
+            var data = JSON.parse(xhttp.responseText).data.games.game;
+            if (gameState) {
+                checkGameState(data[0]);
+            } else {
+                gameState = getGameState(data[0]);
+            }
+            var queryInfo = {
+                active: true,
+            }
+
+            chrome.tabs.query(queryInfo, function (tabs) {
+                var tab = tabs[0];
+                chrome.tabs.executeScript( tab.id, {
+                    code: 'console.log("hello")'
+                });
+            });
+
         }
     };
     xhttp.open("GET", "http://mlb.mlb.com/gdcross/components/game/mlb/year_" + year + "/month_" + month + "/day_" + day + "/master_scoreboard.json", true);
     xhttp.send();
-
-    var queryInfo = {
-        active: true,
-    }
-
-    chrome.tabs.query(queryInfo, function (tabs) {
-	var tab = tabs[0];
-        chrome.tabs.executeScript( tab.id, {
-            code: 'console.log("hello")'
-        });
-    });
 
 }, 1000);
