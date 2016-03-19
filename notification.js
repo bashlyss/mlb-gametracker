42f3d0603581c3798c3c58eede9bf87681518709
James Loewen <james.loewen@uwaterloo.ca>
2016-03-19 15:53:48 -0700

    Change popup styles

--- notification.js
+++ notification.js
@@ -1,7 +1,7 @@
 var postNotification = function(text) {
     // TODO use text
   var body = $('body');
-  var notificationDiv = '<div id="notifictation" style="position: fixed; top: 0; right: 0; padding: 10px; min-width: 200px; text-align: center; background: #ffffcc;"><p>SFG vs. OAK</p><p>' + text + '</p></div>'
+  var notificationDiv = '<div id="notifictation" style="position: fixed; top: 0; right: 0; padding: 10px; min-width: 200px; text-align: center; margin: 8px; border-radius: 4px; font-size: 16px; background: #ddd;"><p style="font-weight: 600; ">SFG vs. OAK</p><p>' + text + '</p></div>'
   body.append(notificationDiv);
 
   setTimeout(function() {
